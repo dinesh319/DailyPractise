f47183af5c17644969013ecea23f2a2f32577aae
dinesh.hanumanthu <dinesh.hanumanthu@infanion.com>
2025-03-01 14:34:59 +0530

    bean life cycle hooks

--- src/main/java/com/example/DailyPractise/basic/Apple.java
+++ src/main/java/com/example/DailyPractise/basic/Apple.java
@@ -1,14 +1,29 @@
 package com.example.DailyPractise.basic;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 //@Component   // Apple has been a bean because of @Component
 public class Apple {
 
+    @PostConstruct
+    public void callBeforeAppleIsUsed(){  // invoked immediately after a bean is constructed and all of
+        // its dependencies are injected
+        System.out.println("wash the apples before eating");
+    }
+
 
     public void eat(){
         System.out.println(" i am eating apple");
     }
+
+    @PreDestroy
+    public void beforeBeanDestroying(){ // invoked just before the bean is destroyed by the container
+        System.out.println("finished about the apple");
+    }
+
+
 
 }
