98532f8023f97cc79398c0d00c9459c887220996
dinesh319 <135752558+dinesh319@users.noreply.github.com>
2025-03-23 13:54:56 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/example/DailyPractise/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/DailyPractise/advice/GlobalExceptionHandler.java
@@ -15,35 +15,34 @@ public class GlobalExceptionHandler {
 
 
 
-    private ApiResponse<ApiError> buildApiResponseWithApiError(ApiError apiError) {
-        return new ApiResponse<>(apiError);
+    private ResponseEntity<ApiResponse<?>> buildResponseEntityWithError(ApiError apiError) {
+        return new ResponseEntity<>(new ApiResponse<>(apiError) , apiError.getStatus());
     }
 
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ApiResponse<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException){
+    public ResponseEntity<ApiResponse<?>>  handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException){
         List<String> errors = methodArgumentNotValidException.getBindingResult().getAllErrors().stream().map(err -> err.getDefaultMessage()).toList();
 
         ApiError apiError = ApiError.builder()
                 .message("validation failed").subMessages(errors).status(HttpStatus.BAD_REQUEST)
                 .build();
 
-        return buildApiResponseWithApiError(apiError);
-    }
+       return  buildResponseEntityWithError(apiError);    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ApiResponse<ApiError> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException){
+    public ResponseEntity<ApiResponse<?>>  handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException){
         ApiError apiError = ApiError.builder()
                 .message(resourceNotFoundException.getMessage()).status(HttpStatus.NOT_FOUND)
                 .build();
-        return buildApiResponseWithApiError(apiError);
-    }
+       return  buildResponseEntityWithError(apiError);    }
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse<ApiError> handleException(Exception exception){
+    public ResponseEntity<ApiResponse<?>>  handleException(Exception exception){
         ApiError apiError = ApiError.builder()
                 .message(exception.getMessage()).status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .build();
-        return buildApiResponseWithApiError(apiError);
+       return  buildResponseEntityWithError(apiError);
     }
 
 
